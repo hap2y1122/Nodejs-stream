ca451135eb4b2240d50d502f507004656696d04e
Swaraj Kumar Chaudhary <b190040@nitsikkim.ac.in>
2021-01-23 20:08:42 +0530

    [Refactor] 01-strm-write.js
    Updated file

--- 01-strm-write.js
+++ 01-strm-write.js
@@ -3,8 +3,12 @@ var fs = require('fs');
 /* create & write stream */
 var write_stream = fs.createWriteStream("./test.txt");
 
-write_stream.write("January \n");
-write_stream.write("February \n");
-write_stream.write("March \n");
-write_stream.write("April ");
+write_stream.on("finish",function () {
+    console.log("File Created successfully!");
+    write_stream.write("January \n");
+    write_stream.write("February \n");
+    write_stream.write("March \n");
+    write_stream.write("April ");
+});
+
 console.log("Success!!")
