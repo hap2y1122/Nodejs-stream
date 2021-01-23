cf6ee7c2a3965580c7a5a5655717e33c9908ae7d
Swaraj Kumar Chaudhary <b190040@nitsikkim.ac.in>
2021-01-23 20:15:51 +0530

    [MOD] 01-strm-write.js
    Updated File

--- 01-strm-write.js
+++ 01-strm-write.js
@@ -3,12 +3,16 @@ var fs = require('fs');
 /* create & write stream */
 var write_stream = fs.createWriteStream("./test.txt");
 
+/* if file is created */
 write_stream.on("finish",function () {
     console.log("File Created successfully!");
-    write_stream.write("January \n");
-    write_stream.write("February \n");
-    write_stream.write("March \n");
-    write_stream.write("April ");
 });
 
-console.log("Success!!")
+/* write sth to file */
+write_stream.write("January\n");
+write_stream.write("February\n");
+write_stream.write("March\n");
+write_stream.write("April");
+
+/* close the stream */
+write_stream.end();
